3673e51e886a65b3521b520fb3982131e4dd36f2
Kgalalelo8 <tshenoloo1095@gmail.com>
2024-10-06 22:32:53 +0200

    add index.html, styles.css and quiz.js

---
+++ ALX_Simple_Quiz/quiz.js
@@ -0,0 +1,10 @@
+function checkAnswer(){const correctAnswer="4;"
+const userAnswer= document.querySelector('input[name-"quiz":checked');
+const feedbackElement=document.getElemntbyId("feedback");
+if (userAnswer) {const userAnswerValue=userAnswer.value
+if (userAnswerValue===correctAnswer){feedbackElement.textContent="Correct! Well done;"}
+else {feedbackElement.textContent="That's incorrect. Try again!;"}
+else {feedbackElement.textContent="please select an answer;"}
+}
+}
+document.getElementById("submit-answer")addEventListener("click", checkAnswer);
